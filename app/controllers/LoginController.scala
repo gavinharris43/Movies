a8514a4027510ac8d1af88ebe4fd7011846fd984
tvaidotas <tadas.vaidotas@gmail.com>
2020-02-27 11:33:00 +0000

    making the GET for login return a template template is created but empty for now

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -1,12 +1,14 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
-import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
 
 @Singleton
 class LoginController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def login() = TODO
+  def login() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.login())
+  }
 
   def loginSubmit() = TODO
 
