bdce1d98c559e73fd8fca843b72cccbbd0163a9f
tvaidotas <tadas.vaidotas@gmail.com>
2020-02-27 11:39:38 +0000

    passing form to the view for displaying the login form

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -1,13 +1,14 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
+import models.LoginDetails
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
 
 @Singleton
 class LoginController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def login() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.login())
+    Ok(views.html.login(LoginDetails.loginForm))
   }
 
   def loginSubmit() = TODO
