e488ce1f650177556ca33637c25c9d6ccf43c510
tvaidotas <tadas.vaidotas@gmail.com>
2020-03-16 11:44:43 +0000

    added return types

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,7 +7,7 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents, authAction: AuthenticationAction) extends AbstractController(cc) {
 
-  def index = authAction {
+  def index: Action[AnyContent] = authAction {
     Ok(views.html.index("Your new application is ready."))
   }
 
--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -2,16 +2,16 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 import models.LoginDetails
-import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents, Request}
 
 @Singleton
 class LoginController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with play.api.i18n.I18nSupport {
 
-  def login() = Action { implicit request: Request[AnyContent] =>
+  def login(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.login(LoginDetails.loginForm))
   }
 
-  def loginSubmit() = Action { implicit request: Request[AnyContent] =>
+  def loginSubmit(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     LoginDetails.loginForm.bindFromRequest.fold({ formWithErrors =>
       BadRequest(views.html.login(formWithErrors))
     }, { loginDetails =>
