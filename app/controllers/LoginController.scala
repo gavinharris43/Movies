ed7af1cf572a532b447152eeefa40eb279fb533e
tvaidotas <tadas.vaidotas@gmail.com>
2020-03-16 10:49:29 +0000

    small typo update

--- app/controllers/LoginController.scala
+++ app/controllers/LoginController.scala
@@ -15,7 +15,7 @@ class LoginController @Inject()(cc: ControllerComponents) extends AbstractContro
     LoginDetails.loginForm.bindFromRequest.fold({ formWithErrors =>
       BadRequest(views.html.login(formWithErrors))
     }, { loginDetails =>
-      if (LoginDetails.checkIfUserIsVali(loginDetails))
+      if (LoginDetails.checkIfUserIsValid(loginDetails))
         Redirect(routes.HomeController.index()).withSession(request.session + ("username" -> loginDetails.username))
       else
         BadRequest("Incorrect username or password")
