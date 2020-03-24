c095c056489d299f39e10d19d7e9fd6559134bd5
Gavin Harris <gavinharris43@googlemail.com>
2020-03-24 14:17:50 +0000

    application running

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,9 +4,9 @@ import javax.inject._
 import play.api.mvc._
 
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with play.api.i18n.I18nSupport{
 
-  def index = Action {
+  def index = Action { implicit Action =>
     Ok(views.html.index("Your new application is ready."))
   }
 
--- app/controllers/TicketBookingController.scala
+++ app/controllers/TicketBookingController.scala
@@ -1,68 +1,66 @@
 package controllers
 
-import javax.inject.Inject
-import models.Bookings
-import play.api.libs.json.Json
-import play.api.mvc.{AbstractController, _}
+
+import javax.inject.{Inject, Singleton}
 import models.JsonFormats._
+import models.Bookings
 import play.api.libs.json.Json
 import play.api.mvc._
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
 import reactivemongo.api.Cursor
 import reactivemongo.play.json._
 import reactivemongo.play.json.collection.{JSONCollection, _}
-import scala.concurrent.{ExecutionContext, Future}
 
+import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class TicketBookingController @Inject()(components: ControllerComponents,
                                         val reactiveMongoApi: ReactiveMongoApi
                                        ) extends AbstractController(components)
-  with MongoController with ReactiveMongoComponent {
+  with MongoController with ReactiveMongoComponents with play.api.i18n.I18nSupport {
 
   implicit def ec: ExecutionContext = components.executionContext
 
   def collection: Future[JSONCollection] = database.map(_.collection[JSONCollection]("bookings"))
 
 
-def index: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-  Ok (views.html.bookings(Bookings.bookings))
-}
+  def index: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.bookings(Bookings.bookings))
+  }
 
-  def submit () = Action.async {
-  implicit request: Request[AnyContent] =>
-  Bookings.bookings.bindFromRequest.fold ( {
-  formWithErrors =>
-  Future.successful (BadRequest (views.html.bookings (formWithErrors) ) )
-}, {
-    booking =>
+  def submit() = Action.async { implicit request: Request[AnyContent] =>
+    Bookings.bookings.bindFromRequest.fold({
+      formWithErrors =>
+        Future.successful(BadRequest(views.html.bookings(formWithErrors)))
+    }, {
+      booking =>
 
-  val cursor: Future[Cursor[Bookings]] = collection.map {
-  _.find (Json.obj ("title" -> booking.title, "dateTime" -> booking.dateTime) ).
-  sort (Json.obj ("created" -> - 1) ).
-  cursor[Bookings] ()
-}
+        val cursor: Future[Cursor[Bookings]] = collection.map {
+          _.find(Json.obj("title" -> booking.title, "dateTime" -> booking.dateTime)).
+            sort(Json.obj("created" -> -1)).
+            cursor[Bookings]()
+        }
 
-  val futureUsersList: Future[List[Bookings]] =
-  cursor.flatMap (
-  _.collect[List] (
-  - 1,
-  Cursor.FailOnError[List[Bookings]] ()
-  )
-  )
+        val futureUsersList: Future[List[Bookings]] =
+          cursor.flatMap(
+            _.collect[List](
+              -1,
+              Cursor.FailOnError[List[Bookings]]()
+            )
+          )
 
-  futureUsersList.map {
-  value =>
-  if (value.headOption.nonEmpty) {
-  collection.flatMap (_.insert.one (booking) ).map {
-  _ => Ok ("booked" )
-}
-  Ok ("booked" )
-}
-  else BadRequest ("No Movie/Viewing found for date and time/ movie selected" )
-}
-})
-}
+        futureUsersList.map {
+          value =>
+            if (value.headOption.nonEmpty) {
+              collection.flatMap(_.insert.one(booking)).map {
+                _ => Ok("booked")
+              }
+              Ok("booked")
+            }
+            else BadRequest("No Movie/Viewing found for date and time/ movie selected")
+        }
+    })
+  }
 
 }
 
