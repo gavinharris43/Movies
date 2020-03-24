04d78f53abcfc28e202f2b15639bcb6c5586f382
Gavin Harris <gavinharris43@googlemail.com>
2020-03-24 12:46:35 +0000

    startend implementation

--- app/controllers/TicketBookingController.scala
+++ app/controllers/TicketBookingController.scala
@@ -1,35 +1,44 @@
 package controllers
 
-import javax.inject._
+import javax.inject.Inject
 import models.Bookings
 import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, _}
-
-import scala.concurrent.Future
+import models.JsonFormats._
+import play.api.libs.json.Json
+import play.api.mvc._
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
+import reactivemongo.api.Cursor
+import reactivemongo.play.json._
+import reactivemongo.play.json.collection.{JSONCollection, _}
+import scala.concurrent.{ExecutionContext, Future}
 
 
 @Singleton
 class TicketBookingController @Inject()(components: ControllerComponents,
                                         val reactiveMongoApi: ReactiveMongoApi
                                        ) extends AbstractController(components)
-  with MongoController with ReactiveMongoComponent
+  with MongoController with ReactiveMongoComponent {
+
+  implicit def ec: ExecutionContext = components.executionContext
+
+  def collection: Future[JSONCollection] = database.map(_.collection[JSONCollection]("bookings"))
 
 
-def index = Action {
-  Ok (views.html.bookings (Bookings.bookings) )
+def index: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+  Ok (views.html.bookings(Bookings.bookings))
 }
 
   def submit () = Action.async {
   implicit request: Request[AnyContent] =>
   Bookings.bookings.bindFromRequest.fold ( {
   formWithErrors =>
-  Future.successful (BadRequest (views.html.signup (formWithErrors) ) )
+  Future.successful (BadRequest (views.html.bookings (formWithErrors) ) )
 }, {
-  person =>
+    booking =>
 
   val cursor: Future[Cursor[Bookings]] = collection.map {
-  _.find (Json.obj ("username" -> person.username) ).
+  _.find (Json.obj ("title" -> booking.title, "dateTime" -> booking.dateTime) ).
   sort (Json.obj ("created" -> - 1) ).
   cursor[Bookings] ()
 }
@@ -44,18 +53,16 @@ def index = Action {
 
   futureUsersList.map {
   value =>
-  if (value.headOption.isEmpty) {
-  collection.flatMap (_.insert.one (person) ).map {
-  _ => Ok (request2Messages.messages ("accountCreated") )
+  if (value.headOption.nonEmpty) {
+  collection.flatMap (_.insert.one (booking) ).map {
+  _ => Ok ("booked" )
 }
-  Ok (request2Messages.messages ("accountCreated") )
+  Ok ("booked" )
 }
-  else BadRequest (request2Messages.messages ("usernameInUser") )
+  else BadRequest ("No Movie/Viewing found for date and time/ movie selected" )
 }
 })
 }
 
 }
 
-
-}
