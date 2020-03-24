6a76747e5278b746f1044643383d6773461c22bb
Gavin Harris <gavinharris43@googlemail.com>
2020-03-24 16:22:02 +0000

    implementation

--- app/models/Bookings.scala
+++ app/models/Bookings.scala
@@ -4,17 +4,17 @@ import akka.http.scaladsl.model.DateTime
 import play.api.data.Form
 import play.api.data.Forms._
 
-case class Bookings(title: String, dateTime: String, name: String, concession :Int, adult:Int, child:Int)
+case class Bookings(movieTitle: String, dateTime: String, bookerName: String, concession: Int, adult: Int, child: Int)
 
 object Bookings {
 
-  val bookings: Form[Bookings] = Form(
+  val bookingForm: Form[Bookings] = Form(
     mapping(
       "movieTitle" -> nonEmptyText,
-      "dateTime " -> nonEmptyText,
-      "bookerName"-> nonEmptyText,
-      "concession" ->  number(min = 0, max = 120),
-      "adult"->  number(min = 0, max = 120),
+      "dateTime" -> nonEmptyText,
+      "bookerName" -> nonEmptyText,
+      "concession" -> number(min = 0, max = 120),
+      "adult" -> number(min = 0, max = 120),
       "child" -> number(min = 0, max = 120)
     )(Bookings.apply)(Bookings.unapply)
   )
