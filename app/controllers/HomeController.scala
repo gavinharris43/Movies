3d3f4d41e686f75760a627fb28d6f46b4f02cb42
Wozack <wozniakjack96@gmail.com>
2020-03-25 14:46:43 +0000

    Added opening times

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -10,4 +10,7 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def openings: Action[AnyContent] = Action{
+    Ok(views.html.openingtimes())
+  }
 }
