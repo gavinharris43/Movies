431401d4f0f5652791a3339977ece90558c61e6b
Your Name <you@example.com>
2020-03-26 14:30:23 +0000

    Form actually works

--- app/models/CardDetails.scala
+++ app/models/CardDetails.scala
@@ -2,13 +2,37 @@ package models
 
 import play.api.data.Form
 import play.api.data.Forms._
+import play.api.libs.json.OFormat
+import reactivemongo.bson.BSONObjectID
 
 import scala.collection.mutable.ListBuffer
 
-case class CardDetails(nameoncard: String, cardnumber: String, expirydate:String, securitycode:String, name:String, address:String , city: String, county: String, country: String, postcode: String,
-                       phone:Long, email:String)
+case class CardDetails(_id: BSONObjectID,
+                       NameOnCard: String,
+                       CardNumber: String,
+                       ExpiryDate: String,
+                       SecurityCode: String,
+                       FullName: String,
+                       Address: String,
+                       City: String,
+                       County: String,
+                       Country: String,
+                       Postcode: String,
+                       Phone: String,
+                       Email: String,
+                      ) {
+  val id: BSONObjectID = _id
+
+}
 
 object CardDetails {
+
+  def apply(CardNumber: String, NameOnCard: String, ExpiryDate: String, SecurityCode: String, FullName: String, Address: String, City: String, County: String, Country: String, Postcode: String, Phone: String, Email: String) =
+    new CardDetails(BSONObjectID.generate(), CardNumber, NameOnCard, ExpiryDate, SecurityCode, FullName, Address, City, County, Country, Postcode, Phone, Email)
+
+  def unapply(arg: CardDetails): Option[( String, String, String, String, String, String, String, String, String, String, String, String)] =
+    Option((arg.NameOnCard, arg.CardNumber, arg.ExpiryDate, arg.SecurityCode, arg.FullName, arg.Address, arg.City, arg.County, arg.Country, arg.Postcode, arg.Phone, arg.Email))
+
 
   val CardForm: Form[CardDetails] = Form(
     mapping(
@@ -22,12 +46,21 @@ object CardDetails {
       "county" -> text.verifying("County Required", _.nonEmpty),
       "country" -> text.verifying("Country Required", _.nonEmpty),
       "postcode" -> text.verifying("Post Code Required", _.nonEmpty),
-      "phone" -> longNumber,
+      "phone" -> text.verifying("Phone Number Required", _.nonEmpty),
       "email" -> text.verifying("E-Mail Required", _.nonEmpty)
 
     )(CardDetails.apply)(CardDetails.unapply)
   )
 
+}
+object JsonFormats {
 
+  import play.api.libs.json.Json
+  import reactivemongo.play.json._
+  import reactivemongo.play.json.collection.JSONCollection
+  import play.api.libs.json._
+
+  implicit val cardFormat: OFormat[CardDetails] = Json.format[CardDetails]
+  implicit val bsonObjectIDFormat: OFormat[BSONObjectID] = Json.format[BSONObjectID]
 
 }
