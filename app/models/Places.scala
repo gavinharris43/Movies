534a4732dd2580960ecb2921e757f4a6f2678be6
Gavin Harris <gavinharris43@googlemail.com>
2020-03-26 17:17:13 +0000

    added add form

--- app/models/Places.scala
+++ app/models/Places.scala
@@ -3,13 +3,14 @@ package models
 import play.api.data.Form
 import play.api.data.Forms._
 
-case class Places(placeName: String, address: String, contact: String, description: String, discount: String, picture: String){
+case class Places(placeType: String,placeName: String, address: String, contact: String, description: String, discount: String, picture: String){
 }
 
 object Places {
 
   val placesForm: Form[Places] = Form(
     mapping(
+      "placeType" -> nonEmptyText,
       "placeName" -> nonEmptyText,
       "address" -> nonEmptyText,
       "contact" -> nonEmptyText,
