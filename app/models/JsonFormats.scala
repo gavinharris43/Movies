582043b65637faebeec0cb8aebc5dc8abd688dfb
Your Name <you@example.com>
2020-03-26 14:42:53 +0000

    Payment rebased

--- app/models/JsonFormats.scala
+++ app/models/JsonFormats.scala
@@ -1,11 +1,17 @@
 package models
 
 import play.api.libs.json.OFormat
+import reactivemongo.bson.BSONObjectID
 
 
 
 object JsonFormats {
   import play.api.libs.json.Json
+  import reactivemongo.play.json._
+  import reactivemongo.play.json.collection.JSONCollection
+  import play.api.libs.json._
   implicit val bsonObjectIDFormat: OFormat[Bookings] = Json.format[Bookings]
   implicit val moviesFormat: OFormat[Movies] = Json.format[Movies]
+  implicit val cardFormat: OFormat[CardDetails] = Json.format[CardDetails]
+  implicit val bsonObjectIDFormats: OFormat[BSONObjectID] = Json.format[BSONObjectID]
 }
