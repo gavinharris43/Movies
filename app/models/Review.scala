ffc36cd6e9ed8e8e737b9fe8d4502604e930a139
juamal <juamal27@gmail.com>
2020-03-27 15:23:48 +0000

    added form

--- app/models/Review.scala
+++ app/models/Review.scala
@@ -8,23 +8,24 @@ import reactivemongo.bson.BSONObjectID
 
 import play.api.data.Forms.{mapping, text}
 
-case class Review(name: String, body: String, rating :String, movieTitle: String)
+case class Review(name: String, body: String, rating : Int, movieTitle: String)
 
 object Review{
-
   def apply(name: String,
             body: String,
-            rating: String,
+            rating: Int,
             movieTitle: String):
   Review = new Review(name, body, rating, movieTitle)
 
+  def unapply(arg: Review):
+  Option[(String, String, Int, String)] = Option((arg.name, arg.body, arg.rating, arg.movieTitle))
 
 
   val ReviewForm: Form[Review] = Form(
     mapping(
       "name" -> nonEmptyText,
       "body" -> nonEmptyText,
-      "rating" -> nonEmptyText,
+      "rating" -> number,
       "movieTitle" -> nonEmptyText,
     )(Review.apply)(Review.unapply)
   )
