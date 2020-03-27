da95f8716ca02d8ffecf4a10bce5c9ef95ae85d2
Dan Parkes <dan.parkes15@gmail.com>
2020-03-27 15:23:04 +0000

    attempt at db

--- app/models/JsonFormats.scala
+++ app/models/JsonFormats.scala
@@ -1,10 +1,16 @@
 package models
 
 import play.api.libs.json.OFormat
+import reactivemongo.bson.BSONObjectID
 
 object JsonFormats {
 
   import play.api.libs.json.Json
+  import reactivemongo.play.json._
+  import reactivemongo.play.json.collection.JSONCollection
+  import play.api.libs.json._
 
   implicit val bsonObjectIDFormat: OFormat[Bookings] = Json.format[Bookings]
+  implicit val ratingFormat: OFormat[Rating] = Json.format[Rating]
+
 }
