09bb13053f3e4935f109ffe2ac62408a3bc417a7
Dan Parkes <dan.parkes15@gmail.com>
2020-03-27 15:59:04 +0000

    db insertions look better, data in files

--- app/models/Rating.scala
+++ app/models/Rating.scala
@@ -7,4 +7,15 @@ case class Rating(title: String,
 object Rating {
   def apply(title: String, iconLocation: String, description: String):
   Rating = new Rating(title, iconLocation, description)
+
+  val ratingU: Rating = Rating("U", "@routes.Assets.versioned('images/classifications/u.png')",
+                                "@routes.Assets.versioned('ratingsText/U')")
+  val ratingPG: Rating = Rating("PG", "@routes.Assets.versioned('images/classifications/pg.png')",
+                                "@routes.Assets.versioned('ratingsText/PG')")
+  val rating12: Rating = Rating("12", "@routes.Assets.versioned('images/classifications/12.png')",
+                                "@routes.Assets.versioned('ratingsText/12')")
+  val rating15: Rating = Rating("15", "@routes.Assets.versioned('images/classifications/15.png')",
+                                "@routes.Assets.versioned('ratingsText/15')")
+  val rating18: Rating = Rating("18", "@routes.Assets.versioned('images/classifications/18.png')",
+                                "@routes.Assets.versioned('ratingsText/18')")
 }
