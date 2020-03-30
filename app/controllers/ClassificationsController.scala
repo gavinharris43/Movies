5f4b349b29916c69a25e13a7930d4c9fa234b19a
Dan Parkes <dan.parkes15@gmail.com>
2020-03-30 14:54:02 +0100

    some db code that doesn't work

--- app/controllers/ClassificationsController.scala
+++ app/controllers/ClassificationsController.scala
@@ -1,40 +1,45 @@
 package controllers
 
 import javax.inject.Inject
+import models.JsonFormats._
 import models.Rating
-import play.api.db
-import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents, Request}
-import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
-import reactivemongo.play.json.collection.JSONCollection
 import play.api.libs.json._
-import reactivemongo.play.json._
+import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
+import reactivemongo.api.Cursor
 import reactivemongo.play.json.JsObjectDocumentWriter
-import reactivemongo.play.json.collection._
+import reactivemongo.play.json.collection.{JSONCollection, _}
 
 import scala.concurrent.{ExecutionContext, Future}
-import models.JsonFormats._
 
 class ClassificationsController @Inject()(cc: ControllerComponents,
                                           val reactiveMongoApi: ReactiveMongoApi)
   extends AbstractController(cc)
-   with MongoController with ReactiveMongoComponents  with play.api.i18n.I18nSupport {
-
-  def classifications: Action[AnyContent] = Action {
-    Ok(views.html.classifications())
-  }
+    with MongoController with ReactiveMongoComponents with play.api.i18n.I18nSupport {
 
   implicit def ec: ExecutionContext = cc.executionContext
+
   def collection: Future[JSONCollection] = database.map(_.collection[JSONCollection]("ratings"))
 
-  def insertRatings(): Action[AnyContent] = Action.async {
-    val futureResult = collection.flatMap(_.insert.many(Seq(Rating.ratingU,
-                                                            Rating.ratingPG,
-                                                            Rating.rating12,
-                                                            Rating.rating15,
-                                                            Rating.rating18)))
-    futureResult.map(_ => Ok("Inserted"))
+  def create: Action[AnyContent] = Action.async {
+    val ratings = Rating.ratings
+    val futureResult = collection.flatMap(_.insert.many(ratings))
+    futureResult.map(_ => Ok("Ratings inserted"))
   }
 
+  def findAll: Future[List[Rating]] = {
+    collection.map {
+      _.find(Json.obj())
+        .cursor[Rating]()
+    }.flatMap(
+      _.collect[List](
+        -1,
+        Cursor.FailOnError[List[Rating]]()
+      )
+    )
+  }
 
-
+  def classificationIndex: Action[AnyContent] = Action.async {
+    findAll.map(ratings => Ok(views.html.classifications(ratings)))
+  }
 }
