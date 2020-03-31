218034b4eba22c1d2e33e45374d444a7cab3cd05
Dan Parkes <dan.parkes15@gmail.com>
2020-03-31 10:09:25 +0100

    changed data to the way it was before

--- app/models/Rating.scala
+++ app/models/Rating.scala
@@ -8,16 +8,29 @@ object Rating {
   def apply(title: String, iconLocation: String, description: String):
   Rating = new Rating(title, iconLocation, description)
 
-  val ratingU : Rating = Rating("U",  "@routes.Assets.versioned('images/classifications/u.png')",
-    "ratingsText/U")
-  val ratingPG: Rating = Rating("PG", "@routes.Assets.versioned('images/classifications/pg.png')",
-    "ratingsText/PG")
-  val rating12: Rating = Rating("12", "@routes.Assets.versioned('images/classifications/12.png')",
-    "ratingsText/12")
-  val rating15: Rating = Rating("15", "@routes.Assets.versioned('images/classifications/15.png')",
-    "ratingsText/15")
-  val rating18: Rating = Rating("18", "@routes.Assets.versioned('images/classifications/18.png')",
-    "ratingsText/18")
+  val ratingU: Rating = Rating("U", "images/classifications/u.png",
+    "Films classified 12A and video works classified 12 contain material that is not " +
+      "generally suitable for children aged under 12. No one younger than 12 may see a 12A film in a " +
+      "cinema unless accompanied by an adult. Adults planning to take a child under 12 to view a 12A " +
+      "film should consider whether the film is suitable for that child. No one younger than 12 may " +
+      "rent or buy a 12 rated video work.")
+  val ratingPG: Rating = Rating("PG", "images/classifications/pg.png",
+    "General viewing, but some scenes may be unsuitable for young children. A PG film " +
+      "should not unsettle a child aged around eight or older. Unaccompanied children of any age may " +
+      "watch, but parents are advised to consider whether the content may upset younger, or more " +
+      "sensitive, children.")
+  val rating12: Rating = Rating("12", "images/classifications/12.png",
+    "Films classified 12A and video works classified 12 contain material that is not " +
+      "generally suitable for children aged under 12. No one younger than 12 may see a 12A film in a " +
+      "cinema unless accompanied by an adult. Adults planning to take a child under 12 to view a 12A " +
+      "film should consider whether the film is suitable for that child. No one younger than 12 may " +
+      "rent or buy a 12 rated video work.")
+  val rating15: Rating = Rating("15", "images/classifications/15.png",
+    "No one younger than 15 may see a 15 film in a cinema. No one younger than 15 may " +
+      "rent or buy a 15 rated video work.")
+  val rating18: Rating = Rating("18", "images/classifications/18.png",
+    "No one younger than 18 may see an 18 film in a cinema. No one younger than 18 may " +
+      "rent or buy an 18 rated video work. Adults should be free to choose their own entertainment.")
 
-  val ratings = Seq(ratingPG, rating12, rating15, rating18, ratingU)
+  val ratings = Seq(ratingU, ratingPG, rating12, rating15, rating18)
 }
