fe524abde24c5e9d3559eb77d226e98846be0237
juamal <juamal27@gmail.com>
2020-04-01 15:21:33 +0100

    added new release

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,13 +1,19 @@
 package controllers
 
+import helpers.FilmDateControl
 import javax.inject._
 import play.api.mvc._
 
-@Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) with play.api.i18n.I18nSupport{
+import scala.concurrent.ExecutionContext.Implicits.global
 
-  def index = Action { implicit Action =>
-    Ok(views.html.index("Your new application is ready."))
+
+
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents, movieController: MovieController, filmDateControl: FilmDateControl) extends AbstractController(cc) with play.api.i18n.I18nSupport{
+
+  def index = Action.async {
+    movieController.findAll().map(movies => Ok(views.html.index(movies.sortWith(filmDateControl.sortByDateTime(_,_)).take(3),"Your new application is ready.")) )
+
   }
 
 
--- app/controllers/ListingController.scala
+++ app/controllers/ListingController.scala
@@ -5,15 +5,18 @@ import javax.inject._
 import models.{FilmDate, Movies}
 import play.api.mvc._
 import scala.concurrent.ExecutionContext.Implicits.global
+import helpers.{FilmDateControl}
+import org.joda.time.{ DateTime, DateTimeZone }
+import org.joda.time.format.DateTimeFormat
 
 @Singleton
-class ListingController @Inject()(cc: ControllerComponents, val movieController: MovieController) extends AbstractController(cc) {
+class ListingController @Inject()(cc: ControllerComponents, val movieController: MovieController, val filmDateControl: FilmDateControl) extends AbstractController(cc) {
 
 
   val personaTest = Movies(
     "Persona 5",
     List[String]("images/film-photos/persona5/img1.jpg", "images/film-photos/persona5/img2.jpg", "images/film-photos/persona5/img3.jpg", "images/film-photos/persona5/img4.jpg"),
-    FilmDate(Tuple3(25,3,1999),Tuple2(11,15)),
+    FilmDate(Tuple3(25,3,2010),Tuple2(11,15)),
     List[String]("Juamal", "Jack"),
     List[String]("Gavin", "Dan", "Kobby")
   )
@@ -21,29 +24,29 @@ class ListingController @Inject()(cc: ControllerComponents, val movieController:
   val sonicTest = Movies(
     "Sonic",
     List[String]("images/film-photos/Sonic/img1.jpg", "images/film-photos/Sonic/img2.jpg", "images/film-photos/Sonic/img3.jpg", "images/film-photos/Sonic/img4.jpg"),
-    FilmDate(Tuple3(25,3,2020),Tuple2(11,15)),
+    FilmDate(Tuple3(25,2,1999),Tuple2(11,15)),
     List[String]("Bob", "Phil"),
     List[String]("Gavin", "Dan", "Kobby")
   )
   val powerRangersTest = Movies(
     "PowerRangers",
     List[String]("images/film-photos/PowerRangers/img1.jpg", "images/film-photos/PowerRangers/img2.jpg", "images/film-photos/PowerRangers/img3.jpg", "images/film-photos/PowerRangers/img4.jpg"),
-    FilmDate(Tuple3(25,11,2020),Tuple2(11,15)),
+    FilmDate(Tuple3(25,5,1999),Tuple2(11,15)),
     List[String]("Bob", "Phil"),
     List[String]("Gavin", "Dan", "Kobby")
   )
 
 
   def listingGalleryIndex = Action.async {
-    //movieController.createMovie(personaTest)
-    //movieController.createMovie(sonicTest)
-    //movieController.createMovie(powerRangersTest)
-    movieController.findAll().map(movies => Ok(views.html.listingGallery(movies)))
+//    movieController.createMovie(personaTest)
+//    movieController.createMovie(sonicTest)
+//    movieController.createMovie(powerRangersTest)
+    movieController.findAll().map(movies => Ok(views.html.listingGallery(movies.sortWith(filmDateControl.sortByDateTime(_,_)))))
   }
-
-
+  def newListingGalleryIndex = Action.async {
+    movieController.findAll().map(movies => Ok(views.html.newListingGallery(movies.sortWith(filmDateControl.sortByDateTime(_,_)).take(6))))
+  }
   def listingIndex(title:String) = Action.async {
-
     movieController.findByName(title).map(film => Ok(views.html.listing(film.head)))
   }
 
