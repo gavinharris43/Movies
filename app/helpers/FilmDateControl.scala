d4270118f536e2eee45a027da00648baeae5741b
juamal <juamal27@gmail.com>
2020-04-01 15:21:33 +0100

    added new release

--- app/helpers/FilmDateControl.scala
+++ app/helpers/FilmDateControl.scala
@@ -1,1 +1,20 @@
+package helpers
 
+import models.Movies
+import org.joda.time.{DateTime, DateTimeZone}
+import org.joda.time.format.DateTimeFormat
+
+
+class FilmDateControl{
+
+  private val DATE_TIME_PATTERN = DateTimeFormat.forPattern("dd/MM/yyyy")
+
+  def getDate(string: String): String ={
+    string.substring(0,10)
+  }
+  def sortByDateTime(film: Movies, film2: Movies): Boolean ={
+    val date1 = DATE_TIME_PATTERN.parseDateTime(getDate(film.dateRelease))
+    val date2 = DATE_TIME_PATTERN.parseDateTime(getDate(film2.dateRelease))
+    if (date1.isAfter(date2)) true else false
+  }
+}
