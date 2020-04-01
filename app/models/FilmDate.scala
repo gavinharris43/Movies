71495e095dac07ca3c798d3a97fd05a0fcab222b
juamal <juamal27@gmail.com>
2020-04-01 16:29:36 +0100

    updated db to use correct dates

--- app/models/FilmDate.scala
+++ app/models/FilmDate.scala
@@ -6,7 +6,6 @@ case class FilmDate(date: Tuple3[Int,Int,Int], time: Tuple2[Int,Int] ){
     else if ((date._1 < 9) && (date._2 > 9)){ (("0"+ date._1+ "/" + date._2 + "/" + date._3 : String) + time) :String}
     else if ((date._1 > 9) && (date._2 < 9)){ ((date._1+ "/" + "0" + date._2 + "/" + date._3 : String) + time) :String}
     else{ ("" + date._1 + "/" + date._2 + "/" + date._3 + time :String)}
-
   }
 }
 
